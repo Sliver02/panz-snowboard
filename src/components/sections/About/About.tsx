5b54c75c127ab1bdecaafc655910c3c2fd7e86ac
Jacopo Panzera <jacopo@goviceversa.com>
2026-09-04 10:31:49 +0200

    redesigned About section with intro paragraphs and a background gradient

--- src/components/sections/About/About.tsx
+++ src/components/sections/About/About.tsx
@@ -1,7 +1,9 @@
 import { Button } from "@/components/atoms/Button";
 import { Col, Container, Row } from "@/components/atoms/Grid";
-import { Align, Justify } from "@/components/atoms/Grid/interfaces";
+import { Align } from "@/components/atoms/Grid/interfaces";
 import { SectionTitle } from "@/components/atoms/SectionTitle";
+import { Paragraphs } from "@/components/atoms/Paragraphs";
+import { FadeIn } from "@/components/atoms/FadeIn";
 import { CardFrame } from "@/components/molecules/CardFrame";
 import { Section } from "@/components/organisms/Section";
 import { RouteEnum } from "@/common/routeEnum";
@@ -9,31 +11,33 @@ import classNames from "classnames";
 import { useTranslations } from "next-intl";
 import Link from "next/link";
 import portraitImage from "@public/images/photo_2024-12-17_23-31-21.jpg";
+import styles from "./About.module.scss";
 
 export const About = () => {
 	const t = useTranslations("about");
 	const general = useTranslations("general");
 
 	return (
-		<Section>
-			<Container>
-				<Row>
-					<Col xs={12} md={6}>
-						<CardFrame image={portraitImage} alt={t("photoAlt")} />
-					</Col>
-					<Col xs={12} md={6}>
-						<Row mdAlign={Align.center}>
-							<Col>
+		<div className={classNames(styles.about)}>
+			<div className={classNames(styles.backgroundGradient)} />
+			<Section>
+				<Container>
+					<Row mdAlign={Align.center}>
+						<Col xs={12} md={6}>
+							<CardFrame image={portraitImage} alt={t("photoAlt")} />
+						</Col>
+						<Col xs={12} md={6}>
+							<FadeIn>
 								<SectionTitle highlight text={t("title")} />
-								<p className={classNames("text--p-lg")}>{general("intro")}</p>
+								<Paragraphs text={general("intro")} />
 								<Link href={RouteEnum.ABOUT}>
 									<Button style={{ marginTop: "1rem" }}>{t("button")}</Button>
 								</Link>
-							</Col>
-						</Row>
-					</Col>
-				</Row>
-			</Container>
-		</Section>
+							</FadeIn>
+						</Col>
+					</Row>
+				</Container>
+			</Section>
+		</div>
 	);
 };
