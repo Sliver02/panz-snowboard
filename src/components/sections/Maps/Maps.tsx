07017fac78a05c24ce0dd9a4e683e612cda12248
Jacopo Panzera <jacopo@goviceversa.com>
2026-09-04 10:31:35 +0200

    swapped the homepage locations map for the interactive LocationsMap

--- src/components/sections/Maps/Maps.tsx
+++ src/components/sections/Maps/Maps.tsx
@@ -3,45 +3,35 @@ import { Col, Container, Row } from "@/components/atoms/Grid";
 import { Align } from "@/components/atoms/Grid/interfaces";
 import { SectionTitle } from "@/components/atoms/SectionTitle";
 import { Section } from "@/components/organisms/Section";
+import { LocationsMap } from "@/components/molecules/LocationsMap";
+import { FadeIn } from "@/components/atoms/FadeIn";
 import classNames from "classnames";
-import Image from "next/image";
-import mapImage from "@public/images/mappa.png";
 import { useTranslations } from "next-intl";
 
 export const Maps = () => {
 	const t = useTranslations("maps");
-	const general = useTranslations("general");
 
 	return (
 		<Section id="locations">
 			<Container>
-				<Row>
-					<Col xs={12}>
-						<SectionTitle text={t("title")} highlight uppercase />
-					</Col>
-				</Row>
 				<Row mdAlign={Align.center}>
-					<Col xs={12} md={6}>
+					<Col xs={12} md={7}>
 						<div
 							style={{
 								position: "relative",
 								width: "100%",
-								height: "400px",
-								marginBottom: "1rem",
+								height: "500px",
 							}}
 						>
-							<Image
-								src={mapImage}
-								placeholder="blur"
-								fill
-								style={{ objectFit: "cover" }} // or 'contain'
-								sizes="100vw" // or specific sizes
-								alt={t("mapAlt")}
-							/>
+							<LocationsMap />
 						</div>
-						<p className={classNames("text--p-lg")}>{general("intro")}</p>
 					</Col>
-					<Col xs={12} md={6}></Col>
+					<Col xs={12} md={4} mdOffset={1}>
+						<FadeIn>
+							<SectionTitle text={t("title")} highlight uppercase />
+							<p className={classNames("text--p-lg")}>{t("description")}</p>
+						</FadeIn>
+					</Col>
 				</Row>
 			</Container>
 		</Section>
